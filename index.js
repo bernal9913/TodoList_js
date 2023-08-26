eadc5490df4fbb318a2163ebcf2b4ba374d201e5
Bernal9913 <ramirez.bernal.jesus1@gmal.com>
2023-08-26 16:46:34 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const readline = require('readline');
+
+let tarea = {
+    done: false,
+    description: 'sacar la basura'
+}
+
+let taskList = [];
+// primer modo: lectura de tareas necesarias
+
+
+// segundo modo: marcar las tareas realizadas
